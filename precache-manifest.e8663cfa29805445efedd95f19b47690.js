3d1b789062f50aa6b7c1516b0aef96c4d5d587f3
Tanguy Freycon <tanguy.freycon@gmail.com>
2023-05-31 17:12:17 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.e8663cfa29805445efedd95f19b47690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63ece6e158bbcfaca718",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/css/main.afac1d89.chunk.css"
+  },
+  {
+    "revision": "63ece6e158bbcfaca718",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/main.63ece6e1.chunk.js"
+  },
+  {
+    "revision": "ee7b3a4fa7f4227e602a",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/1.ee7b3a4f.chunk.js"
+  },
+  {
+    "revision": "a4f070bc9d1e514dd615",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/runtime~main.a4f070bc.js"
+  },
+  {
+    "revision": "7019ea5a34a05a94a3b0f83ac6287ca9",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/index.html"
+  }
+];
