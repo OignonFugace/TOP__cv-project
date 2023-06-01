30c3153e4f11794bff08895e964d1877dc4afb6a
Tanguy Freycon <tanguy.freycon@gmail.com>
2023-06-01 22:15:18 +0200

    feat: Migrate App to functional component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import PersonalInformation from "./components/PersonalInformation";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import "./App.css";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div id="PageApp">
-        <div className="main">
-          <PersonalInformation></PersonalInformation>
-          <Education></Education>
-          <Experience></Experience>
-        </div>
+const App = () => {
+  return (
+    <div id="PageApp">
+      <div className="main">
+        <PersonalInformation></PersonalInformation>
+        <Education></Education>
+        <Experience></Experience>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default App;
+
