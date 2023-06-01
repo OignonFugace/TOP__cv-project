1c04c6384ca07cf7a73bf2e6590b55b7121a6a87
Tanguy Freycon <tanguy.freycon@gmail.com>
2023-06-01 22:38:09 +0200

    feat: Migrate PersonalInformation to functional component and create unique key

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,40 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from "./Input.js";
 
-export default class PersonalInformation extends Component {
-  constructor() {
-    super();
-    this.state = {
-      personalInfo: {
-        fullName: "Full Name",
-        email: "email@example.com",
-        phoneNumber: "+00 000 000 0000",
-      },
-    };
+const PersonalInformation = () => {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "Full Name",
+    email: "email@example.com",
+    phoneNumber: "+00 000 000 0000",
+  });
+
+  const handleUpdatePersonalInfo = (id, text) => {
+    setPersonalInfo({
+      ...personalInfo,
+      [id]: text,
+    });
   }
 
-  handleUpdatePersonalInfo = (id, text) => {
-    this.setState((prevState) => ({
-      personalInfo: {
-        ...prevState.personalInfo,
-        [id]: text,
-      },
-    }));
-  };
-
-  render() {
-    return (
-      <div className="data-block personal-information">
-        <h2>Personal Informations</h2>
-        {Object.entries(this.state.personalInfo).map(([key, value], index) => (
-          <Input
-            key={index}
-            text={value}
-            id={key}
-            onUpdate={this.handleUpdatePersonalInfo}
-          />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="data-block personal-information">
+      <h2>Personal Informations</h2>
+      {Object.entries(personalInfo).map(([key, value]) => (
+        <Input
+          key={key}
+          text={value}
+          id={key}
+          onUpdate={handleUpdatePersonalInfo}
+        />
+      ))}
+    </div>
+  );
 }
+
+export default PersonalInformation;
+
