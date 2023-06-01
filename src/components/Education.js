48123e1f5f2a79a3bec61e9f73c860747c60f3a2
Tanguy Freycon <tanguy.freycon@gmail.com>
2023-06-01 23:59:42 +0200

    refactor: Refactor Education and Experience to be functional components

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import uniqid from "uniqid";
 
@@ -6,105 +6,95 @@ const defaultExperience = {
   id: uniqid(),
   schoolName: "XYZ University",
   studyTitle: "Bachelor's in Computer Science",
-  studyPeriod: "08/2023 - 05/2027",
+  studyPeriod: "08/2023 - 05/2027"
 };
 
-export default class Education extends Component {
-  constructor() {
-    super();
-    this.state = {
-      educationalExperiences: [
-        {
-          ...defaultExperience,
-        },
-      ],
-    };
-  }
+const Education = () => {
+  const [educationalExperiences, setEducationalExperiences] = useState([
+    {
+      ...defaultExperience
+    }
+  ]);
 
-  handleUpdateEducationalExperience = (id, field, text) => {
-    this.setState((prevState) => ({
-      educationalExperiences: prevState.educationalExperiences.map(
-        (experience) =>
-          experience.id === id ? { ...experience, [field]: text } : experience
-      ),
-    }));
-  };
-
-  handleAddEducationalExperience = () => {
-    this.setState((prevState) => ({
-      educationalExperiences: [
-        ...prevState.educationalExperiences,
-        { ...defaultExperience, id: uniqid() },
-      ],
-    }));
-  };
-
-  handleDeleteEducationalExperience = (id) => {
-    this.setState((prevState) => ({
-      educationalExperiences: prevState.educationalExperiences.filter(
-        (experience) => {
-          return experience.id !== id;
-        }
-      ),
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="data-block work-experiences">
-          {this.state.educationalExperiences.map((experience, index) => (
-            <div
-              className="educational-experience experience-section"
-              key={experience.id}
-            >
-              <div className="experience-header">
-                <h3>Educational Experience {index + 1}</h3>
-                <button
-                  onClick={() => {
-                    this.handleDeleteEducationalExperience(experience.id);
-                  }}
-                >
-                  X
-                </button>
-              </div>
-              <Input
-                text={experience.schoolName}
-                onUpdate={(text) =>
-                  this.handleUpdateEducationalExperience(
-                    experience.id,
-                    "schoolName",
-                    text
-                  )
-                }
-              />
-              <Input
-                text={experience.studyTitle}
-                onUpdate={(text) =>
-                  this.handleUpdateEducationalExperience(
-                    experience.id,
-                    "studyTitle",
-                    text
-                  )
-                }
-              />
-              <Input
-                text={experience.studyPeriod}
-                onUpdate={(text) =>
-                  this.handleUpdateEducationalExperience(
-                    experience.id,
-                    "studyPeriod",
-                    text
-                  )
-                }
-              />
-            </div>
-          ))}
-        </div>
-        <button onClick={this.handleAddEducationalExperience}>
-          Add New Education Experience
-        </button>
-      </div>
+  const handleUpdateEducationalExperience = (id, field, text) => {
+    setEducationalExperiences(prevEducationalExperiences =>
+      prevEducationalExperiences.map(experience =>
+        experience.id === id ? { ...experience, [field]: text } : experience
+      )
     );
-  }
-}
+  };
+
+  const handleAddEducationalExperience = () => {
+    setEducationalExperiences(prevEducationalExperiences => [
+      ...prevEducationalExperiences,
+      { ...defaultExperience, id: uniqid() }
+    ]);
+  };
+
+  const handleDeleteEducationalExperience = id => {
+    setEducationalExperiences(prevEducationalExperiences =>
+      prevEducationalExperiences.filter(experience => {
+        return experience.id !== id;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <div className="data-block work-experiences">
+        {educationalExperiences.map((experience, index) => (
+          <div
+            className="educational-experience experience-section"
+            key={experience.id}
+          >
+            <div className="experience-header">
+              <h3>Educational Experience {index + 1}</h3>
+              <button
+                onClick={() => {
+                  handleDeleteEducationalExperience(experience.id);
+                }}
+              >
+                X
+              </button>
+            </div>
+            <Input
+              text={experience.schoolName}
+              onUpdate={text =>
+                handleUpdateEducationalExperience(
+                  experience.id,
+                  "schoolName",
+                  text
+                )
+              }
+            />
+            <Input
+              text={experience.studyTitle}
+              onUpdate={text =>
+                handleUpdateEducationalExperience(
+                  experience.id,
+                  "studyTitle",
+                  text
+                )
+              }
+            />
+            <Input
+              text={experience.studyPeriod}
+              onUpdate={text =>
+                handleUpdateEducationalExperience(
+                  experience.id,
+                  "studyPeriod",
+                  text
+                )
+              }
+            />
+          </div>
+        ))}
+      </div>
+      <button onClick={handleAddEducationalExperience}>
+        Add New Education Experience
+      </button>
+    </div>
+  );
+};
+
+export default Education;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import uniqid from "uniqid";
 
@@ -9,171 +9,148 @@ const defaultExperience = {
   mainTasks: [
     {
       id: uniqid(),
-      text: "Developed and maintained web applications",
+      text: "Developed and maintained web applications"
     },
     {
       id: uniqid(),
-      text: "Collaborated in cross-functional teams to improve software design",
-    },
+      text: "Collaborated in cross-functional teams to improve software design"
+    }
   ],
-  workPeriod: "January 2022 - Present",
+  workPeriod: "January 2022 - Present"
 };
 
-export default class Experience extends Component {
-  constructor() {
-    super();
-    this.state = {
-      workExperiences: [{ ...defaultExperience }],
-    };
-  }
+const Experience = () => {
+  const [workExperiences, setWorkExperiences] = useState([
+    {
+      ...defaultExperience
+    }
+  ]);
 
-  handleUpdateWorkExperience = (id, field, text) => {
-    this.setState((prevState) => ({
-      workExperiences: prevState.workExperiences.map((experience) =>
+  const handleUpdateWorkExperience = (id, field, text) => {
+    setWorkExperiences(prevWorkExperiences =>
+      prevWorkExperiences.map(experience =>
         experience.id === id ? { ...experience, [field]: text } : experience
-      ),
-    }));
+      )
+    );
   };
 
-  handleUpdateTasks = (experienceId, taskId, text) => {
-    this.setState((prevState) => ({
-      workExperiences: prevState.workExperiences.map((experience) =>
+  const handleUpdateTasks = (experienceId, taskId, text) => {
+    setWorkExperiences(prevWorkExperiences =>
+      prevWorkExperiences.map(experience =>
         experience.id === experienceId
           ? {
               ...experience,
-              mainTasks: experience.mainTasks.map((task) =>
+              mainTasks: experience.mainTasks.map(task =>
                 task.id === taskId ? { ...task, text: text } : task
-              ),
+              )
             }
           : experience
-      ),
-    }));
+      )
+    );
   };
 
-  handleAddWorkExperience = () => {
-    this.setState((prevState) => ({
-      workExperiences: [
-        ...prevState.workExperiences,
-        { ...defaultExperience, id: uniqid() },
-      ],
-    }));
+  const handleAddWorkExperience = () => {
+    setWorkExperiences(prevWorkExperiences => [
+      ...prevWorkExperiences,
+      { ...defaultExperience, id: uniqid() }
+    ]);
   };
 
-  handleAddTask = (id) => {
-    this.setState((prevState) => ({
-      workExperiences: prevState.workExperiences.map((experience) => {
+  const handleAddTask = id => {
+    setWorkExperiences(prevWorkExperiences =>
+      prevWorkExperiences.map(experience => {
         return experience.id === id
           ? {
               ...experience,
               mainTasks: [
                 ...experience.mainTasks,
-                { id: uniqid(), text: "Enter New Task" },
-              ],
+                { id: uniqid(), text: "Enter New Task" }
+              ]
             }
           : experience;
-      }),
-    }));
+      })
+    );
   };
 
-  handleDeleteTask = (experienceId, taskId) => {
-    this.setState((prevState) => ({
-      workExperiences: prevState.workExperiences.map((experience) =>
+  const handleDeleteTask = (experienceId, taskId) => {
+    setWorkExperiences(prevWorkExperiences =>
+      prevWorkExperiences.map(experience =>
         experience.id === experienceId
           ? {
               ...experience,
-            mainTasks: experience.mainTasks.filter((task) =>{
-              return task.id !== taskId;
-            }),
+              mainTasks: experience.mainTasks.filter(task => {
+                return task.id !== taskId;
+              })
             }
           : experience
-      ),
-    }));
-  };
-
-  handleDeleteWorkExperience = (id) => {
-    this.setState((prevstate) => ({
-      workExperiences: prevstate.workExperiences.filter((experience) => {
-        return experience.id !== id;
-      }),
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="data-block work-experience">
-          {this.state.workExperiences.map((experience, index) => (
-            <div
-              className="work-experience experience-section"
-              key={experience.id}
-            >
-              <div className="experience-header">
-                <h3>Work Experience {index + 1}</h3>
-                <button
-                  onClick={() => this.handleDeleteWorkExperience(experience.id)}
-                >
-                  X
-                </button>
-              </div>
-              <Input
-                text={experience.companyName}
-                onUpdate={(text) =>
-                  this.handleUpdateWorkExperience(
-                    experience.id,
-                    "companyName",
-                    text
-                  )
-                }
-              />
-              <Input
-                text={experience.positionTitle}
-                onUpdate={(text) =>
-                  this.handleUpdateWorkExperience(
-                    experience.id,
-                    "companyName",
-                    text
-                  )
-                }
-              />
-              <ul>
-                {experience.mainTasks.map((task) => (
-                  <li className="task-item" key={task.id}>
-                    <Input
-                      text={task.text}
-                      onUpdate={(text) =>
-                        this.handleUpdateTasks(experience.id, task.id, text)
-                      }
-                    />
-                    <button
-                      onClick={() =>
-                        this.handleDeleteTask(experience.id, task.id)
-                      }
-                    >
-                      X
-                    </button>
-                  </li>
-                ))}
-              </ul>
-              <button onClick={() => this.handleAddTask(experience.id)}>
-                +
-              </button>
-              <Input
-                text={experience.workPeriod}
-                onUpdate={(text) =>
-                  this.handleUpdateWorkExperience(
-                    experience.id,
-                    "workPeriod",
-                    text
-                  )
-                }
-              />
-            </div>
-          ))}
-        </div>
-        <button onClick={this.handleAddWorkExperience}>
-          Add New Work Experience
-        </button>
-      </div>
+      )
     );
-  }
-}
+  };
+
+  const handleDeleteWorkExperience = id => {
+    setWorkExperiences(prevWorkExperiences =>
+      prevWorkExperiences.filter(experience => {
+        return experience.id !== id;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <div className="data-block work-experience">
+        {workExperiences.map((experience, index) => (
+          <div
+            className="work-experience experience-section"
+            key={experience.id}
+          >
+            <div className="experience-header">
+              <h3>Work Experience {index + 1}</h3>
+              <button onClick={() => handleDeleteWorkExperience(experience.id)}>
+                X
+              </button>
+            </div>
+            <Input
+              text={experience.companyName}
+              onUpdate={text =>
+                handleUpdateWorkExperience(experience.id, "companyName", text)
+              }
+            />
+            <Input
+              text={experience.positionTitle}
+              onUpdate={text =>
+                handleUpdateWorkExperience(experience.id, "positionTitle", text)
+              }
+            />
+            <ul>
+              {experience.mainTasks.map(task => (
+                <li className="task-item" key={task.id}>
+                  <Input
+                    text={task.text}
+                    onUpdate={text =>
+                      handleUpdateTasks(experience.id, task.id, text)
+                    }
+                  />
+                  <button
+                    onClick={() => handleDeleteTask(experience.id, task.id)}
+                  >
+                    X
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <button onClick={() => handleAddTask(experience.id)}>+</button>
+            <Input
+              text={experience.workPeriod}
+              onUpdate={text =>
+                handleUpdateWorkExperience(experience.id, "workPeriod", text)
+              }
+            />
+          </div>
+        ))}
+      </div>
+      <button onClick={handleAddWorkExperience}>Add New Work Experience</button>
+    </div>
+  );
+};
+
+export default Experience;
